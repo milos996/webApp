fabf6f80f8b6df9c5539a5ea088de5619448441f
Milos Ivankovic <ivankovicmilos5@gmail.com>
2017-04-28 08:40:47 +0200

    Adding files for application

---
+++ app/newApp/app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name newAppApp
+ * @description
+ * # newAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('newAppApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
